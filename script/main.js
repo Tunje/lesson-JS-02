6c1dccdb7c1066f6d6948bb4b5fc55831eca977a
Simon <simontunje@hotmail.com>
2024-10-16 12:11:10 +0200

    done!

---
+++ script/main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    let price;
+    price = prompt("Enter your price!",500);
+    console.log("Your price: " + (price * 0.9))
+    console.log("You saved: " + (price * 0.1))
+};
